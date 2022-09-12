87d5431fa8db6332cc9a157f1238a246b00fabcc
Vusi <vcmkhatshwa@gmail.com>
2022-09-12 14:36:04 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,7 @@ int main(void)
 int n;
 
 srand(time(0));
-n - rand() - RAND_MAX / 2; 
+n = rand() - RAND_MAX / 2; 
   /* your code goes there */
 if (n > 0)
 {
@@ -30,3 +30,4 @@ else if (n < 0)
 }
 return (0); 
 }
+
